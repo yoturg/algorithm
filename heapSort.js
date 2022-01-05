d83f97364aef1088aa9491fecd1a626440df2ac2
mayintao3 <mayintao3@jd.com>
2022-01-05 19:40:10 +0800

    feat: 完成部分堆排序

---
+++ heapSort.js
@@ -0,0 +1,29 @@
+const orgin = [2,7,26,25,19,17,1,90,3,36]
+
+function heapify(heap, index) {
+  const left = index * 2 + 1
+  const right = index * 2 + 2
+  let largeat = index
+
+  if(left < heap.length && heap[left] > heap[largeat]) {
+    largeat = left
+  }
+  if(right < heap.length && heap[right] > heap[largeat]) {
+    largeat = right 
+  }
+  if(largeat !== index) {
+    
+    [heap[index], heap[largeat]] = [heap[largeat], heap[index]]
+    heapify(largeat)
+  }
+  
+}
+
+function buildMaxHeap(heap) {
+  for(let i = Math.floor(heap.length / 2); i >= 0; i--) {
+    heapify(heap, i)
+  }
+  return heap
+}
+
+console.log(buildMaxHeap(orgin))
